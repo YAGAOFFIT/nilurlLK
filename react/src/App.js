11c4661c2001fd49e99902ea872c9a9c5876282c
Max Chertkov <mail4maxchertkov@gmail.com>
2023-10-23 21:30:05 +0700

    added login and register pages

--- react/src/App.js
+++ react/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Index from './components/Index';
 import Settings from './components/Settings';
-import Cabinet from './components/Cabinet'
+import Cabinet from './components/Cabinet';
+import Login from './components/Login';
+import Regest from './components/Regest';
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
         <header className="App-header">
         </header>
         <Routes>
-          <Route path="/" element={<Index />} />
           <Route path="/settings" element={<Settings />} />
           <Route path="/cabinet" element={<Cabinet />} />
+          <Route path="/login" element={<Login />} />   
+          <Route path="/regest" element={<Regest />} />               
         </Routes>
       </Router>
     </div>
